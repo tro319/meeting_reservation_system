a00fe89bcc6abbcbfc5535f1a51632f75248d0e1
y.shotaro <shotaro2452@gmail.com>
2025-06-02 20:46:50 +0900

    index.jsp追加、試験用DBConnectパッケージとクラス追加

---
+++ src/main/java/db_connect/DBConnect.java
@@ -0,0 +1,13 @@
+package db_connect;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class DBConnect
+ */
+@WebServlet("/DBConnect")
+public class DBConnect extends HttpServlet {
+       
+
+}
