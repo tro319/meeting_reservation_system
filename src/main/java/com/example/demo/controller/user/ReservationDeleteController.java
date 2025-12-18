0578ada33612a1a20bce96be58f92353a4d10345
よしだ <ktc23a32f0005@edu.kyoto-tech.ac.jp>
2025-12-18 15:35:14 +0900

    2025.12.18 予約管理処理Controllerクラス実装(Service未) 吉田

---
+++ src/main/java/com/example/demo/controller/user/ReservationDeleteController.java
@@ -0,0 +1,60 @@
+package com.example.demo.controller.user;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+
+
+/*
+ * 予約キャンセル処理群
+ * 
+ * @author ys
+ * 
+ */
+
+@Controller
+@RequiredArgsConstructor
+
+public class ReservationDeleteController {
+	
+	private final ReservationDeleteService service;
+	
+	
+	/*　予約キャンセル処理
+	 * 
+	 * @param session セッション値情報
+	 * @param redirectAttributes リダイレクト値情報
+	 * @return 予約一覧取得処理のパスへ
+	 * 
+	 */
+	
+	@GetMapping("/user/reservation_delete")
+	
+	public String deleteReservation(HttpSession session, @RequestParam int id) {
+		
+		Integer loginId = (Integer)session.getAttribute("log_user_id");
+		
+		// ログインしているかどうか
+		
+		if (loginId == null) {
+			
+			return "redirect:/user/login";
+			
+		}
+		
+		Integer reservationId = (Integer)id;
+		
+		service.deleteReservation(reservationId);
+		
+		session.setAttribute("reservation_delete_result", "予約をキャンセルしました");
+		
+		return "redirect:/user/reservations";
+		
+		
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/user/ReservationGetController.java
@@ -0,0 +1,66 @@
+package com.example.demo.controller.user;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.model.entity.Reservation;
+
+import lombok.RequiredArgsConstructor;
+
+
+/*
+ * 予約取得処理群
+ * 
+ * @author ys
+ * 
+ */
+
+@Controller
+@RequiredArgsConstructor
+
+public class ReservationGetController {
+	
+	private final ReservationGetService service;
+	
+	
+	/*　予約取得処理
+	 * 
+	 * @param session セッション値情報
+	 * @param redirectAttributes リダイレクト値情報
+	 * @return 予約一覧表示のパスへ
+	 * 
+	 */
+	
+	@GetMapping("/user/reservation")
+	
+	public String getReservation(HttpSession session, RedirectAttributes redirectAttributes, @RequestParam int id) {
+		
+		Integer loginId = (Integer)session.getAttribute("log_user_id");
+		
+		// ログインしているかどうか
+		
+		if (loginId == null) {
+			
+			return "redirect:/user/login";
+			
+		}
+		
+		
+		Integer reservationId = (Integer)id;
+		
+		Reservation reservationInfo = service.getReservation(reservationId);
+		
+		redirectAttributes.addFlashAttribute("reservation", reservationInfo);
+		
+		session.setAttribute("reservation_id", reservationId);
+		
+		return "redirect:/user/reservation_view";
+		
+		
+	}
+
+}
